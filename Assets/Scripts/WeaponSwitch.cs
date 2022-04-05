9f60163f82833e1569f80b6d12a30eb539592df3
Karol <43847587+MyKarcio123@users.noreply.github.com>
2022-04-05 18:05:14 +0200

    New Gun
    Adding new Gun ( riffle ) and changing weapons ( only by mouse wheel )

---
+++ Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+    int curentWeapon = 0;
+
+    private void Start()
+    {
+        SelectWeapon();
+    }
+    private void Update()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon +1> transform.childCount-1)
+            {
+                selectedWeapon = 0;
+            }
+            else
+            {
+                selectedWeapon++;
+            }
+            SelectWeapon();
+        }
+        else if(Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon -1 < 0)
+            {
+                selectedWeapon = transform.childCount-1;
+            }
+            else
+            {
+                selectedWeapon--;
+            }
+            SelectWeapon();
+        }
+    }
+    void SelectWeapon()
+    {
+        transform.GetChild(curentWeapon).gameObject.SetActive(false);
+        transform.GetChild(selectedWeapon).gameObject.SetActive(true);
+        curentWeapon = selectedWeapon;
+    }
+}
